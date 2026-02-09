ed0cde8c45c75601d5f5f44b96d8c9b67aa09b76
Drew Coffin <ariviom@gmail.com>
2026-02-08 17:12:46 -0800

    Revert viewport-fit: cover and safe-area-inset-bottom handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,6 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  viewportFit: 'cover',
 };
 
 export default async function RootLayout({
